faf880102398b17644f4b4a108b6775b9e8229b3
Mitch Weaver <mitchell.d.weaver@gmail.com>
2015-02-27 16:55:13 -0600

    Basic http server

--- server/main.js
+++ server/main.js
@@ -1,1 +1,10 @@
-console.log('Hello world...LOLZ!');
+var http = require('http'),
+    port = 8888;
+
+console.log('Server listening on port', port);
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Hello World');
+    response.end();
+}).listen(port);
