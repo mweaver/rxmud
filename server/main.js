2bcebc2d44a8d90c0ed25c2f6d959905b60eaddb
Mitch Weaver <mitchell.d.weaver@gmail.com>
2015-02-27 23:10:49 -0600

    simple sockjs chat working

--- server/main.js
+++ server/main.js
@@ -1,10 +1,34 @@
 var http = require('http'),
-    port = 8888;
+    sockjs = require('sockjs'),
+    port = 8888,
+    clients = {},
+    echo = sockjs.createServer(),
+    httpServer = http.createServer();
+
+function broadcast(message) {
+
+    for (var client in clients){
+        // send the message to that client
+        clients[client].write(JSON.stringify(message));
+    }
+}
+
+echo.on('connection', function (conn) {
+    console.log('connected');
+    clients[conn.id] = conn;
+
+    conn.on('data', function(message) {
+        console.log(message);
+        broadcast(JSON.parse(message));
+    });
+
+    conn.on('close', function() {
+        delete clients[conn.id];
+    });
+});
+
+echo.installHandlers(httpServer, { prefix: '/echo' });
+
+httpServer.listen(port, '0.0.0.0');
 
 console.log('Server listening on port', port);
-
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello World');
-    response.end();
-}).listen(port);
